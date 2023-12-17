c6ffa8cec410da6f50a15a70a3c14cdbc7c694ad
Nacho <146840051+rivero04@users.noreply.github.com>
2023-12-17 17:49:56 -0300

    up cahnges

--- execute_command.c
+++ execute_command.c
@@ -1,18 +1,3 @@
-#include "shell.h"
-/**
- * execute_command - Execute a command.
- * @args: Array of strings containing the command and its arguments.
- * @env: Array of strings containing the environment variables.
- *
- * Description:
- * This function forks a new process to execute a command specified in @args.
- * It retrieves the full path of the command using the get_command function.
- * The child process then attempts to execute the command using execve.
- * If successful, the child process is replaced with the specified command.
- * The parent process waits for the child process to complete.
- *
- * Return: No return value.
- */
 int execute_command(char **args)
 {
     pid_t pid;
@@ -21,9 +6,11 @@ int execute_command(char **args)
 
     if (cmd == NULL)
     {
-        printf("Command not found\n");
-        return (-1);
+        fprintf(stderr, "Command not found: %s\n", args[0]);
+        return -1;
     }
+
+    printf("Executing command: %s\n", cmd);
 
     pid = fork();
 
