249c151ee95ebf66835fdbf9187f535d85800438
Nacho <146840051+rivero04@users.noreply.github.com>
2023-12-17 16:34:53 -0300

    Function prototypes

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+char *_getenv(const char *env_var);
+char *get_command(char *command);
+char **split_string(char *buffer, char *del);
+void free_tokens(char **tokens);
+int execute_command(char **args);
+int main(void);
+void printenv(void);
+
+#define PROMPT "(꒡⌓꒡)"
+
+#endif
