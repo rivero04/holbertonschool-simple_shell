ad79913f4acbdb65d7235dabd2d003f4f160bbba
Nacho <146840051+rivero04@users.noreply.github.com>
2023-12-17 17:15:45 -0300

    update

--- execute_command.c
+++ execute_command.c
@@ -48,3 +48,74 @@ int execute_command(char **args)
     free(cmd);
     return status;
 }
+
+/**
+ * Searches for an executable command in the PATH.
+ *
+ * Returns the full path if found, otherwise returns NULL.
+ */
+char *findCommandInPath(char *command_path) {
+    char *path_token, *path = getenv("PATH");
+
+    if (path == NULL) {
+        fprintf(stderr, "Error: PATH environment variable is not set\n");
+        return NULL;
+    }
+
+    path_token = strtok(path, ":");
+    while (path_token != NULL) {
+        char *full_path = malloc(strlen(path_token) + strlen(command_path) + 2);
+        if (full_path == NULL) {
+            fprintf(stderr, "Error: Failed to allocate memory\n");
+            return NULL;
+        }
+
+        sprintf(full_path, "%s/%s", path_token, command_path);
+
+        if (access(full_path, X_OK) == 0) {
+            return full_path;
+        }
+
+        free(full_path);
+        path_token = strtok(NULL, ":");
+    }
+
+    return NULL;
+}
+
+/**
+ * Execute a command using the given command path and arguments.
+ *
+ * Returns the exit status of the command.
+ */
+int executeWithPath(char *command_path, char **array) 
+{
+    int return_value = 0;
+    struct stat perms;
+
+    if (stat(command_path, &perms) == 0) 
+    {
+        return_value = execute_command(array[0], array);
+        return return_value;
+    } 
+    else 
+    {
+        command_path = findCommandInPath(array[0]);
+        if (command_path == NULL) 
+        {
+            printf("./hsh: 1: %s: not found", array[0]);
+            return -1;
+        }
+
+        array[0] = command_path;
+
+        if (stat(array[0], &perms) == 0) 
+        {
+            return_value = execute_command(array[0], array);
+        }
+
+        free(command_path);
+        return return_value;
+    }
+    return -1;
+}
