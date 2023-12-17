24e563c9fbac2ab022699cc9ad2881586eb2a9bf
Nacho <146840051+rivero04@users.noreply.github.com>
2023-12-17 17:53:03 -0300

    up changes

--- execute_command.c
+++ execute_command.c
@@ -1,3 +1,18 @@
+#include "shell.h"
+
+/**
+ * execute_command - Execute a command.
+ * @args: Array of strings containing the command and its arguments.
+ *
+ * Description:
+ * This function forks a new process to execute a command specified in @args.
+ * It retrieves the full path of the command using the get_command function.
+ * The child process then attempts to execute the command using execve.
+ * If successful, the child process is replaced with the specified command.
+ * The parent process waits for the child process to complete.
+ *
+ * Return: No return value.
+ */
 int execute_command(char **args)
 {
     pid_t pid;
@@ -21,6 +36,7 @@ int execute_command(char **args)
     }
     else if (pid == 0)
     {
+        printf("Child process: PID=%d\n", getpid());
         if (execve(cmd, args, environ) == -1)
         {
             perror("execve");
@@ -29,8 +45,11 @@ int execute_command(char **args)
     }
     else
     {
+        printf("Parent process: Waiting for child (PID=%d) to complete...\n", pid);
         waitpid(pid, &status, 0);
+        printf("Parent process: Child process (PID=%d) completed with status %d\n", pid, status);
     }
+
     free(cmd);
     return status;
 }
